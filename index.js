3ece8b48cfe802e937c4d9f98f22d482f57b9499
Artem Glukhanko <v1z.workmail@gmail.com>
2017-09-08 01:12:43 +0300

    refactored

--- index.js
+++ index.js
@@ -27,107 +27,113 @@ const help = [
 
 // connect to DB
 const DB_connect = (uri = URI) =>
-  new Promise((resolve, reject) => {
-    MongoClient.connect(uri)
-      .then(database => resolve(database))
-      .catch(err => reject(err))
-  })
+  MongoClient.connect(uri)
 
 // Insert user with 2 keys into the database
-const DB_insertUser = (user, apiKey, apiSecret, database) =>
-  new Promise((resolve, reject) => {
-    database.collection('users')
-      .insert({
-        user: user,
-        apiKey: apiKey,
-        apiSecret: apiSecret
-      }, (err, result) => {
-        err === null
-          ? resolve()
-          : reject(err)
-      })
-  })
+const DB_insertUser = (userId, apiKey, apiSecret) =>
+  new Promise((resolve, reject) =>
+    DB_connect()
+      .then(database =>
+        database.collection('users')
+          .insert({
+            user: userId,
+            apiKey: apiKey,
+            apiSecret: apiSecret
+          })
+            .then(() => {
+              database.close()
+              resolve()
+            })
+            .catch(err => {
+              database.close()
+              reject(err)
+            })
+      )
+      .catch(err => reject(err))
+  )
 
 // Remove user from DB by UserId (drop user keys - allowing him to register new keys)
-const DB_removeUser = (userId, database) =>
-  new Promise((resolve, reject) => {
-    database.collection('users')
-      .deleteOne({user: userId}, (err, result) => {
-        err === null
-          ? resolve()
-          : reject(err)
-      })
-  })
-
-// Try to find user in database by userId and return true/false
-const DB_isUserInDB = (userId, database) =>
-  new Promise(resolve => {
-    database.collection('users')
-      .find({user: userId})
-      .toArray((err, users) => {
-        err === null
-          ? resolve(users.length == 1)
-          : reject(err)
-      })
-  })
+const DB_removeUser = userId =>
+  new Promise((resolve, reject) =>
+    DB_connect()
+      .then(database =>
+        database.collection('users')
+          .deleteOne({user: userId})
+            .then(() => {
+              database.close()
+              resolve()
+            })
+            .catch(err => {
+              database.close()
+              reject(err)
+            })
+      )
+      .catch(err => reject(err))
+  )
 
 // Return user keys or false if they weren't provided
-const DB_getUserKeys = (userId, database) =>
-  new Promise(resolve => {
-    database.collection('users')
-      .find({user: userId})
-      .toArray((err, user) => {
-        err === null
-          ? resolve(user)
-          : reject(err)
-      })
-  })
+const DB_findUser = userId =>
+  new Promise((resolve, reject) =>
+    DB_connect()
+      .then(database =>
+        database.collection('users')
+          .find({user: userId})
+          .toArray()
+            .then(user => {
+              database.close()
+              resolve(user)
+            })
+            .catch(err => {
+              database.close()
+              reject(err)
+            })
+      )
+      .catch(err => reject(err))
+  )
+
+// Show how many users provided their keys
+const DB_usersCount = () =>
+  new Promise((resolve, reject) =>
+    DB_connect()
+      .then(database =>
+        database.collection('users')
+          .count()
+          .then(count => {
+            database.close()
+            resolve(count)
+          })
+          .catch(err => {
+            database.close()
+            reject(err)
+          })
+      )
+      .catch(err => reject(err))
+  )
 
 //
 // ---------- LOGIC METHODS ----------
 //
 
-// Show how many users provide their keys
-const getUsersCount = database =>
-  new Promise((resolve, reject) => {
-    database.collection('users')
-      .find({})
-      .toArray((err, users) => {
-        err === null
-          ? resolve(users.length)
-          : reject(err)
-      })
-  })
-
 // Register new user by adding new user with 2 keys to users object
-const registerUser = (user, apiKey, apiSecret) =>
-    new Promise(resolve => {
+const registerUser = (userId, apiKey, apiSecret) =>
+    new Promise((resolve, reject) => {
       let response = 'Sometheing go wrong, try again later'
 
       // If user already provided keys to DB - change response msg
-      DB_connect()
-        .then(database => {
-          DB_isUserInDB(user, database)
-            .then(isUserInDB => {
-              database.close()
-
-              if (isUserInDB)
-                response = 'You are already registered your keys\n\nSend /keys to show them\n\nOr /clear to remove them'
-            })
+      DB_findUser()
+        .then(user => {
+          if (user.length == 1)
+            response = 'You are already registered your keys\n\nSend /keys to show them\n\nOr /clear to remove them'
         })
         .then(() => {
           // If user isn't in DB - insert his keys
           // If keys is not valid - change response msg
           if (apiKey.length == 32 && apiSecret.length == 32 && response == 'Sometheing go wrong, try again later') {
-            DB_connect()
-              .then(database => {
-                DB_insertUser(user, apiKey, apiSecret, database)
-                  .then(() => {
-                    database.close()
-
-                    resolve('Your keys accepted!\n\n/balance now available for you, enjoy')
-                  })
+            DB_insertUser(userId, apiKey, apiSecret)
+              .then(() => {
+                resolve('Your keys accepted!\n\n/balance now available for you, enjoy')
               })
+              .catch(err => reject(err))
           } else if (apiKey.length != 32 || apiSecret.length != 32) {
             resolve('You provided invalid keys. Check their length to be 32 characters')
           } else {
@@ -137,84 +143,58 @@ const registerUser = (user, apiKey, apiSecret) =>
     })
 
 // Send private keys to user
-const showKeys = user =>
-  new Promise(resolve => {
-    DB_connect()
-      .then(database => {
-        DB_isUserInDB(user, database)
-          .then(isUserInDB => {
-            database.close()
-
-            return isUserInDB
-          })
-          .then(isUserInDB => {
-            if (isUserInDB) {
-              DB_connect()
-                .then(database => {
-                  DB_getUserKeys(user, database)
-                    .then(result => {
-                      database.close()
-
-                      resolve({
-                        apiKey: result[0].apiKey,
-                        apiSecret: result[0].apiSecret
-                      })
-                    })
-                })
-            } else {
-                resolve({})
-              }
-          })
-      })
-  })
-
-// Delete user's keys from users object
-const removeKeys = user =>
-  new Promise(resolve => {
-    DB_connect()
-      .then(database => {
-        DB_removeUser(user, database)
-          .then(() => {
-            database.close()
-
-            resolve()
-          })
-      })
+const showKeys = userId =>
+  DB_findUser(userId)
+    .then(user => {
+      if (user.length == 1)
+        return({
+          apiKey: user[0].apiKey,
+          apiSecret: user[0].apiSecret
+        })
+      else
+        return({})
     })
+    .catch(err => `Sometheing went wrong - ${err}`)
 
 // Send a request to Bittrex for user balance and return an object
-const getUserBalance = (apiKey, apiSecret) => {
-  bittrex.options({
-    'apikey' : apiKey,
-    'apisecret' : apiSecret
-  })
+const getUserBalance = (apiKey, apiSecret) =>
+  new Promise((resolve, reject) => {
+    bittrex.options({
+      'apikey' : apiKey,
+      'apisecret' : apiSecret
+    })
 
-  return new Promise(resolve => {
-    bittrex.getbalances((data, err ) => {
-      resolve(data.result)
+    bittrex.getbalances((data, err) => {
+      if (err === null)
+        resolve(data.result)
+      else
+        reject(err)
     })
   })
-}
 
 // All the magic is here!
-const parseBalance = data =>
-  new Promise(resolve => {
-    const coins = getCoinsArray(data)
+const parseBalance = balance =>
+  new Promise((resolve, reject) => {
+    const coins = balanceToCoinArray(balance)
     const coinsRate = coins.map(coin => getCoinToBTC(coin))
 
-    let coinsToBTC = {"BTC": 1}
-
     getUSDTforBTC()
-      .then((BTCrate) => {
+      .then(BTCrate => {
         Promise.all(coinsRate)
-          .then(rates => rates.map((rate, index) => {
-            coinsToBTC[`${coins[index]}`] = rate
-          }))
-          .then(() => {
+          .then(rates =>
+            rates
+              .reduce((acc, rate, index) => {
+                acc[`${coins[index]}`] = rate
+                return acc
+              }, {})
+          )
+          .then(coinsToBTC => {
+            coinsToBTC['BTC'] = 1
+
             let summaryBTC = 0
             let summaryUSDT = 0
 
-            const coinsBalances = data.reduce((acc, coin) => {
+            const coinsBalances = balance.reduce((acc, coin) => {
               if (coin.Balance != 0) {
                 const currency = coin.Currency
                 const amount = coin.Balance
@@ -225,25 +205,21 @@ const parseBalance = data =>
                 summaryUSDT += +amountUSDT
 
                 return acc += `${currency}: ${amount}\n${amountBTC} BTC\n${amountUSDT} USDT\n\n`
-              } else {
-                return acc
-              }
+              } else return acc
             }, '')
 
-            return coinsBalances + `Total:\n${summaryBTC.toFixed(4)} BTC\n${summaryUSDT.toFixed(2)} USDT`
+            resolve(coinsBalances + `Total:\n${summaryBTC.toFixed(4)} BTC\n${summaryUSDT.toFixed(2)} USDT`)
           })
-          .then(parsedBalance => resolve(parsedBalance))
+          .catch(err => reject(err))
       })
+      .catch(err => reject(err))
   })
 
-
 // Parse user balance object and return array of coin literals (['BCC', 'ETH', ...])
-const getCoinsArray = data =>
-  data.reduce((acc, coin) => {
-    if (coin.Currency != 'BTC')
-      acc.push(coin.Currency)
-    return acc
-  }, [])
+const balanceToCoinArray = balance =>
+  balance
+    .map(coin => coin.Currency)
+    .filter(coin => coin != 'BTC')
 
 // Get last tick (price in BTC) of chosen coin
 const getCoinToBTC = coin =>
@@ -257,11 +233,14 @@ const getCoinToBTC = coin =>
 
 // Get last USDT-BTC price
 const getUSDTforBTC = () =>
-  new Promise(resolve => {
+  new Promise((resolve, reject) => {
     const url = `https://bittrex.com/api/v1.1/public/getticker?market=USDT-BTC`
 
     bittrex.sendCustomRequest(url, (data, err) => {
-      resolve(data.result.Last)
+      if (err == null)
+        resolve(data.result.Last)
+      else
+        reject(err)
     })
   })
 
@@ -286,7 +265,7 @@ bot.onText(/\/help/, msg => {
 })
 
 // /reg
-bot.onText(/\/reg (.+) (.+)/, (msg, match) => {
+bot.onText(/\/reg (.+) (.+)/, (msg, match) =>
   registerUser(String(msg.from.id), String(match[1]), String(match[2]))
     .then(response =>
       bot.sendMessage(
@@ -294,78 +273,106 @@ bot.onText(/\/reg (.+) (.+)/, (msg, match) => {
         response
       )
     )
-})
+    .catch(err =>
+      bot.sendMessage(
+        msg.from.id,
+        `Sometheing went wrong - ${err}`
+      )
+    )
+)
 
 // /me
-bot.onText(/\/me/, msg => {
+bot.onText(/\/me/, msg =>
   bot.sendMessage(
     msg.from.id,
     msg.from.id
   )
-})
+)
 
 // /users
 bot.onText(/\/users/, msg => {
-  if (msg.from.id == MY_USER_ID) {
-    DB_connect()
-      .then(database => {
-        getUsersCount(database)
-          .then(result => {
-            database.close()
-
-            bot.sendMessage(
-              msg.from.id,
-              `Total users: ${result}`
-            )
-          })
-      })
-  }
+  if (msg.from.id == MY_USER_ID)
+    DB_usersCount()
+      .then(count =>
+        bot.sendMessage(
+          msg.from.id,
+          `Total users: ${count}`
+        )
+      )
+      .catch(err =>
+        bot.sendMessage(
+          msg.from.id,
+          `Sometheing went wrong - ${err}`
+        )
+      )
 })
 
 // /keys
-bot.onText(/\/keys/, msg => {
+bot.onText(/\/keys/, msg =>
   showKeys(String(msg.from.id))
-    .then(keys => {
-      if (keys.apiKey && keys.apiSecret) {
+    .then(response => {
+      if (typeof response == 'string')
         bot.sendMessage(
           msg.from.id,
-          `Your apiKey: ${keys.apiKey}\n\nYour apiSecret: ${keys.apiSecret}`
+          response
         )
-      } else {
+      else if (response.apiKey && response.apiSecret)
+        bot.sendMessage(
+          msg.from.id,
+          `Your apiKey: ${response.apiKey}\n\nYour apiSecret: ${response.apiSecret}`
+        )
+      else
         bot.sendMessage(
           msg.from.id,
           'You should register your keys first!\n\n/howto may be helpful'
         )
-      }
     })
-})
+)
 
 // /balance
 bot.onText(/\/balance/, msg => {
   const userId = msg.from.id
 
   showKeys(String(userId))
-    .then(keys => {
-      if (keys.apiKey && keys.apiSecret) {
-        getUserBalance(keys.apiKey, keys.apiSecret)
-          .then(messyBalance => parseBalance(messyBalance))
-          .then(parsedBalance =>
+    .then(response => {
+      if (typeof response == 'string')
+        bot.sendMessage(
+          userId,
+          response
+        )
+      else if (response.apiKey && response.apiSecret)
+        getUserBalance(response.apiKey, response.apiSecret)
+          .then(messyBalance =>
+            parseBalance(messyBalance)
+              .then(parsedBalance =>
+                bot.sendMessage(
+                  userId,
+                  parsedBalance
+                )
+              )
+              .catch(err =>
+                bot.sendMessage(
+                  userId,
+                  `Sometheing went wrong - ${err}`
+                )
+              )
+          )
+          .catch(err =>
             bot.sendMessage(
               userId,
-              parsedBalance
+              `Sometheing went wrong - ${err}`
             )
           )
-      } else {
+      else
         bot.sendMessage(
           userId,
           'You should register your keys first!\n\n/howto may be helpful'
         )
-      }
     })
 })
 
 // /btc
-bot.onText(/\/btc/, msg => {
+bot.onText(/\/btc/, msg =>
   getUSDTforBTC()
     .then(result =>
       bot.sendMessage(
@@ -373,18 +380,30 @@ bot.onText(/\/btc/, msg => {
         `1 BTC = ${result} USDT`
       )
     )
-})
+    .catch(err =>
+      bot.sendMessage(
+        msg.from.id,
+        `Sometheing went wrong - ${err}`
+      )
+    )
+)
 
 // /clear
-bot.onText(/\/clear/, msg => {
-  removeKeys(String(msg.from.id))
+bot.onText(/\/clear/, msg =>
+  DB_removeUser(String(msg.from.id))
     .then(() =>
       bot.sendMessage(
         msg.from.id,
         'Your keys were successfully removed'
       )
     )
-})
+    .catch(err =>
+      bot.sendMessage(
+        msg.from.id,
+        `Sometheing went wrong - ${err}`
+      )
+    )
+)
 
 // /howto
 bot.onText(/\/howto/, msg => {
@@ -399,9 +418,9 @@ bot.onText(/\/howto/, msg => {
 })
 
 // /donate
-bot.onText(/\/donate/, msg => {
+bot.onText(/\/donate/, msg =>
   bot.sendMessage(
     msg.from.id,
     DONATE_TO
   )
-})
+)
