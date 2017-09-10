fe8c5d7158d1b5faa626249ee04b04aa4def779e
Artem Glukhanko <v1z.workmail@gmail.com>
2017-09-10 21:58:18 +0300

    added eslint // refactored register function

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const TelegramBot = require('node-telegram-bot-api')
-const bittrex = require('node.bittrex.api')
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+const TelegramBot = require('node-telegram-bot-api');
+const bittrex = require('node.bittrex.api');
+const { MongoClient } = require('mongodb');
 
-const MY_USER_ID = 1234567
-const TOKEN = '1234567'
-const URI = "1234567"
-const DONATE_TO = '1234567'
+const MY_USER_ID = 1234567;
+const TOKEN = '1234567';
+const URI = '1234567';
+const DONATE_TO = '1234567';
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+const bot = new TelegramBot(TOKEN, { polling: true });
 
 const help = [
   '/help - call the police (joke!)',
@@ -18,133 +17,106 @@ const help = [
   '/keys - show your keys pair',
   '/balance - show your Bittrex balance',
   '/btc - show last USDT price for BTC',
-  '/donate - show the BTC wallet where you can send your donations'
-]
+  '/donate - show the BTC wallet where you can send your donations',
+];
 
 //
 // ---------- MongoDB METHODS ----------
 //
 
 // connect to DB
-const DB_connect = (uri = URI) =>
-  MongoClient.connect(uri)
+const DBConnect = (uri = URI) =>
+  MongoClient.connect(uri);
 
 // Insert user with 2 keys into the database
-const DB_insertUser = (userId, apiKey, apiSecret) =>
-  new Promise((resolve, reject) =>
-    DB_connect()
-      .then(database =>
-        database.collection('users')
-          .insert({
-            user: userId,
-            apiKey: apiKey,
-            apiSecret: apiSecret
-          })
-            .then(() => {
-              database.close()
-              resolve()
-            })
-            .catch(err => {
-              database.close()
-              reject(err)
-            })
-      )
-      .catch(err => reject(err))
-  )
+const DBInsertUser = (userId, apiKey, apiSecret) =>
+  DBConnect()
+    .then(database =>
+      database.collection('users')
+        .insert({
+          user: userId,
+          apiKey,
+          apiSecret,
+        })
+        .then(() => database.close())
+        .catch((err) => {
+          database.close();
+          console.log(err);
+        }))
+    .catch(err => console.log(err));
 
 // Remove user from DB by UserId (drop user keys - allowing him to register new keys)
-const DB_removeUser = userId =>
-  new Promise((resolve, reject) =>
-    DB_connect()
-      .then(database =>
-        database.collection('users')
-          .deleteOne({user: userId})
-            .then(() => {
-              database.close()
-              resolve()
-            })
-            .catch(err => {
-              database.close()
-              reject(err)
-            })
-      )
-      .catch(err => reject(err))
-  )
+const DBRemoveUser = userId =>
+  DBConnect()
+    .then(database =>
+      database.collection('users')
+        .deleteOne({ user: userId })
+        .then(() => database.close())
+        .catch((err) => {
+          database.close();
+          console.log(err);
+        }))
+    .catch(err => console.log(err));
 
 // Return user keys or false if they weren't provided
-const DB_findUser = userId =>
-  new Promise((resolve, reject) =>
-    DB_connect()
-      .then(database =>
-        database.collection('users')
-          .find({user: userId})
-          .toArray()
-            .then(user => {
-              database.close()
-              resolve(user)
-            })
-            .catch(err => {
-              database.close()
-              reject(err)
-            })
-      )
-      .catch(err => reject(err))
-  )
+const DBFindUser = userId =>
+  DBConnect()
+    .then(database =>
+      database.collection('users')
+        .find({ user: userId })
+        .toArray()
+        .then((user) => {
+          database.close();
+          return user;
+        })
+        .catch((err) => {
+          database.close();
+          console.log(err);
+        }))
+    .catch(err => console.log(err));
 
 // Show how many users provided their keys
-const DB_usersCount = () =>
-  new Promise((resolve, reject) =>
-    DB_connect()
-      .then(database =>
-        database.collection('users')
-          .count()
-          .then(count => {
-            database.close()
-            resolve(count)
-          })
-          .catch(err => {
-            database.close()
-            reject(err)
-          })
-      )
-      .catch(err => reject(err))
-  )
+const DBUsersCount = () =>
+  DBConnect()
+    .then(database =>
+      database.collection('users')
+        .count()
+        .then((count) => {
+          database.close();
+          return count;
+        })
+        .catch((err) => {
+          database.close();
+          console.log(err);
+        }))
+    .catch(err => console.log(err));
 
 //
 // ---------- LOGIC METHODS ----------
 //
 
 // Register new user by adding new user with 2 keys to users object
-const registerUser = (userId, apiKey, apiSecret) =>
-    new Promise((resolve, reject) => {
-      let response = 'Sometheing go wrong, try again later'
+const registerUser = (userId, apiKey, apiSecret) => {
+  // If keys are not valid - return error msg
+  if (apiKey.length !== 32 || apiSecret.length !== 32) {
+    return Promise.resolve('You provided invalid keys. Check their length to be 32 characters');
+  }
 
-      // If user already provided keys to DB - change response msg
-      DB_findUser()
-        .then(user => {
-          if (user.length == 1)
-            response = 'You are already registered your keys\n\nSend /keys to show them\n\nOr /clear to remove them'
-        })
-        .then(() => {
-          // If user isn't in DB - insert his keys
-          // If keys is not valid - change response msg
-          if (apiKey.length == 32 && apiSecret.length == 32 && response == 'Sometheing go wrong, try again later') {
-            DB_insertUser(userId, apiKey, apiSecret)
-              .then(() => {
-                resolve('Your keys accepted!\n\n/balance now available for you, enjoy')
-              })
-              .catch(err => reject(err))
-          } else if (apiKey.length != 32 || apiSecret.length != 32) {
-            resolve('You provided invalid keys. Check their length to be 32 characters')
-          } else {
-            resolve(response)
-          }
-        })
-    })
+  return DBFindUser(userId)
+    .then((user) => {
+      // If user already provided keys to DB - return error msg
+      if (user.length === 1) {
+        return 'You are already registered your keys\n\nSend /keys to show them\n\nOr /clear to remove them';
+      }
+      // If user isn't in DB - insert his keys into DB and return welcome msg
+      return DBInsertUser(userId, apiKey, apiSecret)
+        .then(() => 'Your keys accepted!\n\n/balance now available for you, enjoy');
+    });
+};
 
 // Send private keys to user
 const showKeys = userId =>
-  DB_findUser(userId)
+  DBFindUser(userId)
     .then(user => {
       if (user.length == 1)
         return({
@@ -267,18 +239,12 @@ bot.onText(/\/help/, msg => {
 // /reg
 bot.onText(/\/reg (.+) (.+)/, (msg, match) =>
   registerUser(String(msg.from.id), String(match[1]), String(match[2]))
-    .then(response =>
+    .then(response => {
       bot.sendMessage(
         msg.from.id,
         response
       )
-    )
-    .catch(err =>
-      bot.sendMessage(
-        msg.from.id,
-        `Sometheing went wrong - ${err}`
-      )
-    )
+    })
 )
 
 // /me
@@ -292,7 +258,7 @@ bot.onText(/\/me/, msg =>
 // /users
 bot.onText(/\/users/, msg => {
   if (msg.from.id == MY_USER_ID)
-    DB_usersCount()
+    DBUsersCount()
       .then(count =>
         bot.sendMessage(
           msg.from.id,
@@ -390,7 +356,7 @@ bot.onText(/\/btc/, msg =>
 
 // /clear
 bot.onText(/\/clear/, msg =>
-  DB_removeUser(String(msg.from.id))
+  DBRemoveUser(String(msg.from.id))
     .then(() =>
       bot.sendMessage(
         msg.from.id,
